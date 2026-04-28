1fbb048e70d322bc417a365f56e4c4ece60a666c
NguyenManhTuan <nguyentantienblog01@gmail.com>
2026-04-28 08:49:56 +0700

    refactor:Tuan update folder Backend

--- chatbot_api/test_agent.py
+++ chatbot_api/test_agent.py
@@ -33,16 +33,23 @@ def run_test(query: str):
         print(f"\n[THÔNG BÁO HỆ THỐNG]:\n{result['error']}")
     
     print("\n[DỮ LIỆU KẾT QUẢ]:")
+    class CustomEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, Decimal):
+                return float(obj)
+            return super().default(obj)
+
+
     data = result.get("sql_result")
     if data:
-        print(json.dumps(data, indent=2, ensure_ascii=False))
+        print(json.dumps(data, indent=2, ensure_ascii=False, cls=CustomEncoder))
     else:
         print("Không có dữ liệu trả về.")
     print("\n" + "="*50)
 
 if __name__ == "__main__":
     # Test 1: Câu hỏi hợp lệ
-    run_test("Cho tôi biết tổng sản lượng của chuyền LINE_01 ngày 2026-04-22?")
+    run_test("Tỉ lệ lỗi của chuyền 1 là bao nhiêu?")
     
     # Test 2: Câu hỏi không có trong nghiệp vụ (Để test nhánh Error)
     # run_test("Giá vàng hôm nay bao nhiêu?")
